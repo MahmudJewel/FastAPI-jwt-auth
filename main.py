7791653a8653995b3c9f07d79dcbe40444a5231b
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2024-01-09 13:24:32 -0500

    user created

--- main.py
+++ main.py
@@ -21,3 +21,14 @@ def get_db():
 @app.get("/")
 def initialize():
     return {"msg": "Initialization"}
+
+@app.post("/user/", response_model = schemas.User)
+def create_user( user: schemas.UserCreate , db: Session = Depends(get_db)):
+    return functions.create_user(db=db, user=user)
+
+# @app.post("/users/", response_model=schemas.User)
+# def create_users(user: schemas.UserCreate, db: Session = Depends(get_db)):
+#     # db_user = functions.get_user_by_email(db, email=user.email)
+#     # if db_user:
+#     #     raise HTTPException(status_code=400, detail="Email already registered")
+#     return functions.create_user(db=db, user=user)
